605a11224283d395f2c62832f82e14210650bf70
chastine1 <batamurizashastine@gmail.com>
2019-02-06 12:20:52 +0200

    sace

--- src/app/search.ts
+++ src/app/search.ts
@@ -1,3 +1,3 @@
 export class Search {
-    constructor(public avatar_url:string,public name:string,public public_repos:number,public followers:number,public following:number,public html_url:string){}
+    constructor(public avatar_url:string,public name:string,public public_repos:number,public followers:number,public following:number,public html_url:string,){}
 }
